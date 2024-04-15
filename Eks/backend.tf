401d30f3339b129b119d039fc9669ded20b09fff
Kishan5965 <harikishan.r5965@gmail.com>
2024-04-15 14:09:00 +0530

    adding files

--- Eks/backend.tf
+++ Eks/backend.tf
@@ -1,12 +1,5 @@
 terraform {
-    required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.74.0"
-    }
-  }
-
-  backend "s3" {
+     backend "s3" {
     bucket = "eks-bknd"
     key    = "eks/terraform.tfstate"
     region = "ap-south-1"
